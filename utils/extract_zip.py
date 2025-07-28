61c349f1492c66f2519f2a3f579f0539044aa1ac
Gusemplass <guimichaud81@gmail.com>
2025-07-28 16:34:50 +0200

    Update extract_zip.py

--- utils/extract_zip.py
+++ utils/extract_zip.py
@@ -1,1 +1,6 @@
-# Extraction et tri du zip
+import zipfile
+import os
+
+def extract_zip_content(zip_path, output_dir):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)# Extraction et tri du zip
