b14c23c84a54c718ba394ecca24d0ed05715bb9e
Gusemplass <guimichaud81@gmail.com>
2025-07-28 16:39:57 +0200

    Update parse_dpgf.py

--- utils/parse_dpgf.py
+++ utils/parse_dpgf.py
@@ -1,1 +1,13 @@
-# Lecture DPGF
+#import pandas as pd
+import os
+
+def parse_dpgf_excel(folder):
+    for file in os.listdir(folder):
+        if file.endswith(".xlsx"):
+            filepath = os.path.join(folder, file)
+            try:
+                df = pd.read_excel(filepath)
+                return df.fillna("").to_dict(orient="records")
+            except Exception as e:
+                print(f"Erreur lecture DPGF : {e}")
+    return [] Lecture DPGF
